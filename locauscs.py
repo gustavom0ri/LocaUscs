a1fc5625fd4e376509a97e9aa1ba46165bee2ff1
gustavom0rigit config --global --listgit config --global --listgit config --global user.name gustavom0ri <gustavomori382@gmail.com>
2025-05-29 20:41:48 -0300

    Codigo funcional

--- locauscs.py
+++ locauscs.py
@@ -441,6 +441,6 @@ def logout():
 
 
 if __name__ == '__main__':
-    import os
+    print("PORT:", os.environ.get("PORT"))
     port = int(os.environ.get('PORT', 5000))  # Pega a porta do Render
     app.run(host='0.0.0.0', port=port)        # Aceita conexões externas
